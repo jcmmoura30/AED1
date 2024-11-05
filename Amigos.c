498a8697ba22abf109482e9aab7d1d7c7e5c1286
Juan Carlo Monteiro de Moura <jcm.moura30@unifesp.br>
2024-11-05 20:12:44 -0300

    Create Amigos.c

---
+++ Amigos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+struct cel{
+    int conteudo;
+    struct cel*seg;
+}
+int main()
+{
+    char nomes1[50], nomes2[50], nomes3[50];
+    char *novo;
+
+    scanf("%s, %s, %s", &nomes1, &nomes2, &nomes3);
+    
+
+    cel *lst;
+    lst = NULL;
+
+return 0;
+}
