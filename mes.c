bba1163a3025c6e347d8e839bf4744da284e0888
Juan Carlo Monteiro de Moura <jcm.moura30@unifesp.br>
2025-01-12 19:48:36 -0300

    Create mes.c

---
+++ mes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+
+    scanf("%d", &month);
+
+    const char* months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+
+    if (month >= 1 && month <= 12) {
+        printf("%s\n", months[month - 1]);
+    } else {
+        printf("Invalid month\n");
+    }
+
+    return 0;
+}
